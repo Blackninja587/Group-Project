a3d05fc32e2abd14627c7de71234d5f4429e634d
Blackninja587 <sambrangman@gmail.com>
2022-11-15 12:43:47 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+let express = require('express');
+let router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'Home'
+  });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { 
+    title: 'Home'
+  });
+});
+
+module.exports = router;
