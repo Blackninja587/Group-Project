befed96aad6581b8fdf1dfa0fa9b333768eb96ff
Blackninja587 <sambrangman@gmail.com>
2022-11-22 12:37:27 -0500

    deleting old copies

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('This is a user page');
-});
-
-module.exports = router;
