796111d883ddb2cfd43ea7a1f8c015faeff34273
Josh1007 <91282402+Josh1007@users.noreply.github.com>
2022-12-03 00:50:30 -0500

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,13 @@
+let mongoose = require('mongoose');
 
+let passportLocalMongoose = require('passport-local-mongoose');
+
+let User = mongoose.Schema({
+    username:
+    {
+        type:String,
+        default:"",
+        trim:true,
+        required:'Username is Required'
+    },
+})
